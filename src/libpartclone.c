3af76e04ecbc2a7b3ba3a6e5d92b464697bf0da6
Patryck Rouleau <pfrouleau@gmail.com>
2013-09-04 23:21:28 -0400

    Extract generate_usedblocks_map() from v1_verify()
    Signed-off-by: Patryck Rouleau <pfrouleau@gmail.com>

--- src/libpartclone.c
+++ src/libpartclone.c
@@ -282,6 +282,42 @@ v1_read_bitmap(pc_context_t *pcp)
     return(error);
 }
 
+int
+generate_usedblocks_map(pc_context_t *pcp, u_int64_t* nsetp)
+{
+    int error;
+    v1_context_t *v1p = (v1_context_t *) pcp->pc_verdep;
+
+    if ((error =
+	 posix_malloc(&v1p->v1_sumcount,
+		      ((pcp->pc_desc.fs.totalblock >> v1p->v1_bitmap_factor)+1) *
+		      sizeof(u_int64_t))) == 0) {
+	/*
+	 * Precalculate the count of preceding valid blocks
+	 * for every 1k blocks.
+	 */
+	u_int64_t i, nset = 0;
+	for (i=0; i<pcp->pc_desc.fs.totalblock; i++) {
+	    if ((i & ((1<<v1p->v1_bitmap_factor)-1)) == 0) {
+		v1p->v1_sumcount[i>>v1p->v1_bitmap_factor] = nset;
+	    }
+	    /* [2011-08]
+	     * ...sigh... the *bitmap* can have more than
+	     * two values.  It can be 1, in which case it's
+	     * definitely in the file.  It can be zero
+	     * in which case, it's definitely not in the
+	     * file.  And it can be anything else that fits
+	     * into a byte?  What does it mean?  I don't
+	     * know.  But it's not set.
+	     */
+	    if (v1p->v1_bitmap[i] == 1)
+		nset++;
+	}
+	*nsetp = nset;
+    }
+    return(error);
+}
+
 /*
  * v1_verify	- Verify the currently open file.
  *
@@ -303,79 +339,53 @@ v1_verify(pc_context_t *pcp)
 	 */
 	if ((error = posix_malloc(&v1p->v1_bitmap,
 				  pcp->pc_desc.fs.totalblock)) == 0) {
-	    u_int64_t r_size;
+	    u_int64_t nset;
 
-	    if ((error = v1_read_bitmap(pcp) == 0)) {
-		if ((error =
-		     posix_malloc(&v1p->v1_sumcount,
-				  ((pcp->pc_desc.fs.totalblock >>
-				   v1p->v1_bitmap_factor)+1) *
-				  sizeof(u_int64_t))) == 0) {
-			/*
-			 * Precalculate the count of preceding valid blocks
-			 * for every 1k blocks.
-			 */
-			u_int64_t i, nset = 0;
-			for (i=0; i<pcp->pc_desc.fs.totalblock; i++) {
-				if ((i & ((1<<v1p->v1_bitmap_factor)-1)) == 0) {
-				    v1p->v1_sumcount[i>>v1p->v1_bitmap_factor] =
-				    nset;
-			}
-			/* [2011-08]
-			 * ...sigh... the *bitmap* can have more than
-			 * two values.  It can be 1, in which case it's
-			 * definitely in the file.  It can be zero
-			 * in which case, it's definitely not in the
-			 * file.  And it can be anything else that fits
-			 * into a byte?  What does it mean?  I don't
-			 * know.  But it's not set.
-			 */
-			if (v1p->v1_bitmap[i] == 1)
-			    nset++;
-		    }
-		    /*
-		     * Fixup device size...
-		     */
-		    i = pcp->pc_desc.fs.totalblock *
-			pcp->pc_desc.fs.block_size;
-		    if (pcp->pc_desc.fs.device_size != i)
-			pcp->pc_desc.fs.device_size = i;
+	    if ((error = v1_read_bitmap(pcp) == 0) &&
+		(error = generate_usedblocks_map(pcp, &nset)) == 0) {
+		/*
+		 * Fixup device size...
+		 */
+		u_int64_t device_size;
 
-		    /*
-		     * Is the count of used blocks good?
+		device_size = pcp->pc_desc.fs.totalblock *
+			      pcp->pc_desc.fs.block_size;
+		if (pcp->pc_desc.fs.device_size != device_size)
+		    pcp->pc_desc.fs.device_size  = device_size;
+
+		/*
+		 * Is the count of used blocks good?
+		 */
+		if (pcp->pc_desc.fs.usedblocks != nset) {
+		    /* [2011-08]
+		     * What should we do in this case?  The old
+		     * version punted, thinking that it is an
+		     * inconsistency.  However, at the time of
+		     * header construction it may not be possible
+		     * to get a definitive count of used blocks
+		     * without scanning the entire bitmap.  So
+		     * some filesystems use a derived count, which
+		     * can be off.  So, we don't turn on
+		     * STRICT_HEADERS (for now).
 		     */
-		    if (pcp->pc_desc.fs.usedblocks != nset) {
-			/* [2011-08]
-			 * What should we do in this case?  The old
-			 * version punted, thinking that it is an
-			 * inconsistency.  However, at the time of
-			 * header construction it may not be possible
-			 * to get a definitive count of used blocks
-			 * without scanning the entire bitmap.  So
-			 * some filesystems use a derived count, which
-			 * can be off.  So, we don't turn on
-			 * STRICT_HEADERS (for now).
-			 */
 #ifdef	STRICT_HEADERS
-			error = EFAULT;
+		    error = EFAULT;
 #else	/* STRICT_HEADERS */
-			/* what?! - Fix it up silently. */
-			pcp->pc_desc.fs.usedblocks = nset;
+		    /* what?! - Fix it up silently. */
+		    pcp->pc_desc.fs.usedblocks = nset;
 #endif	/* STRICT_HEADERS */
-		    }
-		    if (!error && pcp->pc_cf_handle) {
-			/*
-			 * Verify the change file, if present.
-			 */
-			error = cf_verify(pcp->pc_cf_handle);
-			if (!error)
-			    pcp->pc_flags |= PC_CF_VERIFIED;
-		    }
 		}
-	    } else {
-		if (error == 0)
-		    error = EINVAL;
+		if (!error && pcp->pc_cf_handle) {
+		    /*
+		     * Verify the change file, if present.
+		     */
+		    error = cf_verify(pcp->pc_cf_handle);
+		    if (!error)
+			pcp->pc_flags |= PC_CF_VERIFIED;
+		}
 	    }
+	} else {
+	    error = EINVAL;
 	}
     }
     return(error);
